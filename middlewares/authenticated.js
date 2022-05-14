e9ce44b61e3b3da6e9179ee9b023618a86fde0b9
Syihab Muchsin <hab@muchsin.id>
2022-05-14 07:29:42 +0700

    fix lint

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -5,9 +5,9 @@ const authenticated = async (req, res, next) => {
     const { authorization } = req.headers;
 
     if (!authorization) {
-      return res.status(403).json({ message: 'Invalid Token' })
+      return res.status(403).json({ message: 'Invalid Token' });
     }
-    const token = authorization.split(' ')[1]
+    const token = authorization.split(' ')[1];
     const decoded = jwt.verify(token, 'secret');
     req.user = decoded;
     next();
